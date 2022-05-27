a9c444f76016c400f4e0d0e8645a783e5fb35848
Donte Ladatto <donte279@gmail.com>
2022-05-27 10:54:26 -0500

    commit tree 5c45ce2d9029b3cd831592b7997bec3b2d7d0770

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map{ |name| badge_maker(name) }
+end
+
+def assign_rooms(arr)
+    arr.map.with_index |name| do
+        "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+end
